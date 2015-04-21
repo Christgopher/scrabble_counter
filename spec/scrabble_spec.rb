90bfaee48b725be9f92be383025a89eb1e5d94a8
John Young & Christoper LeCompte <student@epicodus.com>
2015-04-21 14:28:26 -0700

    Complete word functionality

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -4,5 +4,8 @@ require('scrabble')
 describe('String#scrabble') do
   it("returns a scrabble score for a letter") do
     expect("c".scrabble()).to(eq(3))
-end
+  end
+  it("returns a scrabble score for a word") do
+    expect("ax".scrabble()).to(eq(9))
+  end
 end
